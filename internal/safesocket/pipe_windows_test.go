c275eae12cc05a856fbb8cfbd1e38d413a5f3fa7
Nick Khyl <nickk@tailscale.com>
2025-12-15 15:04:01 -0600

    internal/safesocket: use Identification impersonation level for named pipes on Windows
    Also enable tests to run on Windows unprivileged.
    
    Corresponds to tailscale/tailscale@95671b71a6ac2d456bd73ac79f4a4bc71d2ab36a and tailscale/tailscale@f844791e151d1db73277727fffb1c9ab062471bd
    
    Fixes #6
    Fixes #12
    Updates #13
    Updates tailscale/tailscale#18212
    
    Signed-off-by: Nick Khyl <nickk@tailscale.com>

---
+++ internal/safesocket/pipe_windows_test.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2020 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package safesocket
+
+import (
+	"fmt"
+	"testing"
+
+	"golang.org/x/sys/windows"
+)
+
+func init() {
+	// downgradeSDDL is a test helper that downgrades the windowsSDDL variable if
+	// the currently running user does not have sufficient priviliges to set the
+	// SDDL.
+	downgradeSDDL = func() (cleanup func()) {
+		// The current default descriptor can not be set by mere mortal users,
+		// so we need to undo that for executing tests as a regular user.
+		if !isCurrentProcessElevated() {
+			var orig string
+			orig, windowsSDDL = windowsSDDL, ""
+			return func() { windowsSDDL = orig }
+		}
+		return func() {}
+	}
+}
+
+// isCurrentProcessElevated returns true when the current process is
+// running with an elevated token, implying Administrator access.
+func isCurrentProcessElevated() bool {
+	token, err := windows.OpenCurrentProcessToken()
+	if err != nil {
+		return false
+	}
+	defer token.Close()
+
+	return token.IsElevated()
+}
+
+// TestExpectedWindowsTypes is a copy of TestBasics specialized for Windows with
+// type assertions about the types of listeners and conns we expect.
+func TestExpectedWindowsTypes(t *testing.T) {
+	t.Cleanup(downgradeSDDL())
+	const sock = `\\.\pipe\tailscale-test`
+	ln, port, err := Listen(sock, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := fmt.Sprintf("%T", ln), "*safesocket.winIOPipeListener"; got != want {
+		t.Errorf("got listener type %q; want %q", got, want)
+	}
+
+	errs := make(chan error, 2)
+
+	go func() {
+		s, err := ln.Accept()
+		if err != nil {
+			errs <- err
+			return
+		}
+		ln.Close()
+
+		wcc, ok := s.(*WindowsClientConn)
+		if !ok {
+			s.Close()
+			errs <- fmt.Errorf("accepted type %T; want WindowsClientConn", s)
+			return
+		}
+		if wcc.winioPipeConn.Fd() == 0 {
+			t.Error("accepted conn had unexpected zero fd")
+		}
+		if wcc.token == 0 {
+			t.Error("accepted conn had unexpected zero token")
+		}
+
+		s.Write([]byte("hello"))
+
+		b := make([]byte, 1024)
+		n, err := s.Read(b)
+		if err != nil {
+			errs <- err
+			return
+		}
+		t.Logf("server read %d bytes.", n)
+		if string(b[:n]) != "world" {
+			errs <- fmt.Errorf("got %#v, expected %#v\n", string(b[:n]), "world")
+			return
+		}
+		s.Close()
+		errs <- nil
+	}()
+
+	go func() {
+		s := DefaultConnectionStrategy(sock)
+		s.UsePort(port)
+		c, err := Connect(s)
+		if err != nil {
+			errs <- err
+			return
+		}
+		c.Write([]byte("world"))
+		b := make([]byte, 1024)
+		n, err := c.Read(b)
+		if err != nil {
+			errs <- err
+			return
+		}
+		if string(b[:n]) != "hello" {
+			errs <- fmt.Errorf("got %#v, expected %#v\n", string(b[:n]), "hello")
+		}
+		c.Close()
+		errs <- nil
+	}()
+
+	for range 2 {
+		if err := <-errs; err != nil {
+			t.Fatal(err)
+		}
+	}
+}
